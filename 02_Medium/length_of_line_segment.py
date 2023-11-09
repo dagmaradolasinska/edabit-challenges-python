93e1eed8680b5f86724c1b434006664feeda7778
Dagmara Dolasinska <dagmara.dolasinska@gmail.com>
2023-11-09 12:13:00 +0100

    add solution

---
+++ 02_Medium/length_of_line_segment.py
@@ -0,0 +1,6 @@
+# Write a function that takes coordinates of two points on a two-dimensional plane and returns the length of the line segment connecting those two points.
+
+import math
+
+def line_length(dot1, dot2):	
+	return round(math.sqrt((dot2[0] - dot1[0])**2 + (dot2[1] - dot1[1])**2), 2)
